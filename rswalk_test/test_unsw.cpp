8d37cd7d924888eb6e33bb83c93c4709ced70fd5
danpak94 <danpak94@gmail.com>
2017-06-07 22:25:46 -0400

    dummy initialization values to test makeJoints

--- rswalk_test/test_unsw.cpp
+++ rswalk_test/test_unsw.cpp
@@ -1,106 +1,12 @@
 #include <Walk2014Generator.hpp>
-#include <WalkEnginePreProcessor.hpp>
-//#include <RSWalkModule2014.h> // error with flabuffers.h
 #include <iostream>
 using namespace std;
 
-void test_Walk2014Generator(Walk2014Generator myWalk) {
-	cout << "myWalk.isActive():  " << myWalk.isActive() << endl;
-	cout << "myWalk.isStanding():  " << myWalk.isStanding() << endl;
-	myWalk.reset();
-	myWalk.stop();
-	myWalk.readOptions("dummy"); // does nothing
-}
-
-void test_makeJoints(Walk2014Generator myWalk) {
-//TODO: fix result.angles and result.temperatures to be not "nan"
-	ActionCommand::All request;
-	Odometry odometry(0.0f, 0.0f, 0.0f);
-	SensorValues sensors;
-	BodyModel bodyModel;
-	float ballX = 0.0f;
-	float ballY = 0.0f;
-
-	JointValues result = myWalk.makeJoints(&request, 
-						&odometry, 
-						sensors, 
-						bodyModel,
-						ballX,
-						ballY);
-	
-	cout << "makeJoints result :  " << result.temperatures[0] << endl;
-}
-
-void test_WalkEnginePreProcessor_Constructor(WalkEnginePreProcessor myPreProcessor) {
-	cout<< "myPreProcessor.isActive():  " << myPreProcessor.isActive() << endl;
-	myPreProcessor.reset();
-	myPreProcessor.stop();
-	cout << "myPreProcessor.isLinedUp():  " << myPreProcessor.isLinedUp() << endl;
-	myPreProcessor.resetLinedUp();
-	cout << "myPreProcessor.linedUp:  " << myPreProcessor.linedUp << endl;
-	cout << "myPreProcessor.isKicking:  " << myPreProcessor.isKicking << endl;
-	myPreProcessor.lineUpEngine;
-}
-
-void test_WalkEnginePreProcessor_LineUpEngine(WalkEnginePreProcessor myPreProcessor) {
-	WalkEnginePreProcessor::LineUpEngine myLineUpEngine(myPreProcessor.walkEngine);
-	cout << "myLineUpEngine.hasStarted:  " << myLineUpEngine.hasStarted << endl;
-	myLineUpEngine.reset();
-	myLineUpEngine.start(myLineUpEngine.foot);
-
-	ActionCommand::All request;
-	float ballX = 0.0f;
-	float ballY = 0.0f;
-	bool isSideKick = false;
-	bool hasEnded = myLineUpEngine.hasEnded(&request, ballX, ballY, isSideKick);
-	cout << "myLineUpEngine.hasEnded:  " << hasEnded << endl;
-	myLineUpEngine.preProcess(&request, ballX, ballY);
-	cout << "myLineUpEngine.linedUp:  " << myLineUpEngine.linedUp << endl;
-	cout << "myLineUpEngine.linedUpCt:  " << myLineUpEngine.linedUpCt << endl;
-}
-
-void test_WalkEnginePreProcessor_DribbleEngine(WalkEnginePreProcessor myPreProcessor) {
-	WalkEnginePreProcessor::DribbleEngine myDribbleEngine(myPreProcessor.walkEngine);
-	cout << WalkEnginePreProcessor::DribbleEngine::TURN << endl;
-	myDribbleEngine.reset();
-	cout << "myDribbleEngine.hasEnded():  " << myDribbleEngine.hasEnded() << endl;
-
-	myDribbleEngine.start(myDribbleEngine.foot);
-	
-	ActionCommand::All request;
-	BodyModel bodyModel;
-	myDribbleEngine.preProcess(&request, bodyModel);
-}
-
-//void test_RSWalkModule2014(RSWalkModule2014 myRSWalkModule) {
-//	myRSWalkModule.specifyMemoryDependency();
-//	myRSWalkModule.specifyMemoryBlocks();
-//	myRSWalkModule.initSpecificModule();
-//
-//	ActionCommand::Body body;
-//	myRSWalkModule.processTargetModeWalk(body);
-//	myRSWalkModule.processWalkRequest(body);
-//	myRSWalkModule.processFrame();
-//	myRSWalkModule.readOptions("dummy");
-//	myRSWalkModule.handleStepIntoKick();
-//
-//	
-//	//myRSWalkModule.writeDataToFile();
-//}
-
 int main() {
 	cout<<"hello world"<<endl;
-	
-	//Walk2014Generator myWalk;
-	//test_Walk2014Generator(myWalk);
-
-	//WalkEnginePreProcessor myPreProcessor;
-	//test_WalkEnginePreProcessor_DribbleEngine(myPreProcessor);
-
-	
-
-	cout << "done" << endl;
-
+	Walk2014Generator myWalk;
+	cout<<myWalk.isActive()<<endl;
+	double sol = 1 + 1;
 	return 0;
 }
 // Walk2014Generator instance = Walk2014Generator.initilise();
--- rswalk_test/test_walkGenerator.cpp
+++ rswalk_test/test_walkGenerator.cpp
@@ -2,6 +2,62 @@
 #include <iostream>
 using namespace std;
 
+enum Sensor {
+  gyroX,
+  gyroY,
+  gyroZ,
+  accelX,
+  accelY,
+  accelZ,
+  angleX,
+  angleY,
+  angleZ,
+  battery,
+  fsrLFL,
+  fsrLFR,
+  fsrLRL,
+  fsrLRR,
+  fsrRFL,
+  fsrRFR,
+  fsrRRL,
+  fsrRRR,
+  bumperLL,
+  bumperLR,
+  bumperRL,
+  bumperRR,
+  centerButton,
+  headFront,
+  headMiddle,
+  headRear,
+  NUM_SENSORS
+};
+
+enum Joint {
+  HeadYaw,
+  HeadPitch,
+  LHipYawPitch,
+  LHipRoll,
+  LHipPitch,
+  LKneePitch,
+  LAnklePitch,
+  LAnkleRoll,
+  RHipYawPitch,
+  RHipRoll,
+  RHipPitch,
+  RKneePitch,
+  RAnklePitch,
+  RAnkleRoll,
+  LShoulderPitch,
+  LShoulderRoll,
+  LElbowYaw,
+  LElbowRoll,
+  RShoulderPitch,
+  RShoulderRoll,
+  RElbowYaw,
+  RElbowRoll,
+  NUM_JOINTS
+};
+
 void test_Walk2014Generator(Walk2014Generator myWalk) {
 	cout << "myWalk.isActive():  " << myWalk.isActive() << endl;
 	cout << "myWalk.isStanding():  " << myWalk.isStanding() << endl;
@@ -12,31 +68,84 @@ void test_Walk2014Generator(Walk2014Generator myWalk) {
 
 void test_makeJoints(Walk2014Generator myWalk) {
 //TODO: fix result.angles and result.temperatures to be not "nan"
-	ActionCommand::Head head;
-	ActionCommand::Body body;
 	
+	ActionCommand::Head head;
+
+	ActionCommand::Body body;
+
+	body.actionType = ActionCommand::Body::STAND; 
+	body.forward = 0;
+	body.left = 0;
+	body.turn = 0;
+	body.isFast = false;
+	body.bend = 1;
+	body.power = 1;
+
+	ActionCommand::LED leds;
+
+	float sonar = 0.0;
+
 	ActionCommand::All request(head, body, leds, sonar);
-	Odometry odometry(0.0f, 0.0f, 0.0f);
+
+	Odometry odo = Odometry(0.5f, 0.5f, 0.1f); //x, y, rotation
+
 	SensorValues sensors;
+
+	for (int ut_ind=0; ut_ind<NUM_JOINTS; ut_ind++){
+		if (ut_ind != RHipYawPitch){ // RS does not have this joint because RHYP is same as LHYP on Nao
+			// Conversion not needed for testing
+			// int rs_ind = utJointToRSJoint[ut_ind];
+
+			//TODO: figure out joints_ and robot_joint_signs (something related to Module.h)
+			//sensors.joints.angles[ut_ind] = joints_->values_[ut_ind] * robot_joint_signs[ut_ind]; // changed from raw_joints - Josiah
+			//sensors.joints.temperatures[ut_ind] = sensors_->joint_temperatures_[ut_ind];
+			sensors.joints.angles[ut_ind] = ut_ind;
+			sensors.joints.temperatures[ut_ind] = ut_ind * 10;
+		}
+	}
+
 	BodyModel bodyModel;
+
+	Kinematics kinematics;
+
+	kinematics.setSensorValues(sensors);
+	kinematics.updateDHChain();  // 5.2 update bodyModel
+
+	bodyModel.kinematics = &kinematics;
+	bodyModel.update(&odo, sensors);
+
+	// 3.2 Sensors
+    for (int ut_ind=0; ut_ind<bumperRR + 1; ut_ind++){
+        //Conversion not needed for testing
+        //int rs_ind = utSensorToRSSensor[ut_ind];
+        //sensors.sensors[ut_ind] = sensors_->values_[ut_ind];
+        sensors.sensors[ut_ind] = ut_ind * 0.05f;
+    }
+
 	float ballX = 0.0f;
 	float ballY = 0.0f;
 
 	JointValues result = myWalk.makeJoints(&request, 
-						&odometry, 
+						&odo, 
 						sensors, 
 						bodyModel,
 						ballX,
 						ballY);
-	
-	cout << "makeJoints result :  " << result.temperatures[0] << endl;
+
+	// result.angles
+	// result.temperatures
+	// result.stiffnesses
+
+	for (int i=0; i<NUM_JOINTS; i++) {
+		cout << i << ":    " << result.angles[i] << endl;
+	}
 }
 
 int main() {
 	cout<<"START"<<endl;
 
 	Walk2014Generator myWalk;
-	
+	test_makeJoints(myWalk);
 
 	cout << "DONE" << endl;
 
