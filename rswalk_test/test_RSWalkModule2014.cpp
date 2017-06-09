bff478cccc5957804ccc366eb598d27ace1af3f8
danpak94 <danpak94@gmail.com>
2017-06-08 22:03:35 -0400

    adding test_RSWalkModule2014.cpp

---
+++ rswalk_test/test_RSWalkModule2014.cpp
@@ -0,0 +1,25 @@
+#include <RSWalkModule2014.h>
+
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+void test_processFrame(RSWalkModule2014 myRS) {
+	myRS.initSpecificModule();
+
+	while (1) {
+		myRS.processFrame();
+		sleep(1);
+	}
+
+}
+
+int main() {
+	cout << "hello hello" << endl;
+
+	RSWalkModule2014 myRS;
+	test_processFrame(myRS);
+
+	return 0;
+}
